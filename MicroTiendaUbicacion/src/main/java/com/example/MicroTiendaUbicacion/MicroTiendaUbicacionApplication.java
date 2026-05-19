7f4f38b683ae0b0b81ff5afc1c6b698cd4e70fd9
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:33:53 -0400

    Add files via upload

---
+++ MicroTiendaUbicacion/src/main/java/com/example/MicroTiendaUbicacion/MicroTiendaUbicacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroTiendaUbicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroTiendaUbicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroTiendaUbicacionApplication.class, args);
+	}
+
+}
---
+++ MicroTiendaUbicacion/src/test/java/com/example/MicroTiendaUbicacion/MicroTiendaUbicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroTiendaUbicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroTiendaUbicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
